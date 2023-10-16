f00c33bf854c30c6f2d24e491f3afbb7b9e02cb5
tomlenek <lequangminh2204@gmail.com>
2023-10-17 01:08:55 +0700

    FE005 update contact about

--- sdn-bird-farm-shop/src/components/Pages/Contact/Contact.jsx
+++ sdn-bird-farm-shop/src/components/Pages/Contact/Contact.jsx
@@ -1,34 +1,92 @@
 import React from "react";
-import Bird1 from "../../../assets/img/bird-2.jpg"
-import "./Contact.css"
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
+import CommonSection from "../../UI/CommonSection/CommonSection";
 
-function Contact() {
+import "./contact.css";
+
+const socialLinks = [
+    {
+        url: "#",
+        icon: "ri-facebook-line",
+    },
+    {
+        url: "#",
+        icon: "ri-instagram-line",
+    },
+    {
+        url: "#",
+        icon: "ri-twitter-line",
+    },
+];
+
+const Contact = () => {
     return (
-        <div className="contact">
-            <div
-                className="leftSide"
-                style={{ backgroundImage: `url(${Bird1})` }}
-            ></div>
-            <div className="rightSide">
-                <h3>Have a question about a product, our company, or just want to chat? Email us!</h3>
-                <p>We will be glad to assist you in any question and encourage you to share your ideas and improvements with us.</p>
-                <form id="contact-form" method="POST">
-                    <label htmlFor="name">Name</label>
-                    <input name="name" placeholder="Name" type="text" />
-                    <label htmlFor="email">Email</label>
-                    <input name="email" placeholder="Email" type="email" />
-                    <label htmlFor="message">Message</label>
-                    <textarea
-                        rows="6"
-                        placeholder="Message"
-                        name="message"
-                        required
-                    ></textarea>
-                    <button type="submit"> Send Message</button>
-                </form>
-            </div>
+        <div className="Contact">
+            <CommonSection title="Liên Hệ" />
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg="7" md="7">
+                            <h6 className="fw-bold mb-4">Thông tin</h6>
+
+                            <Form>
+                                <FormGroup className="contact__form">
+                                    <Input placeholder="Your Name" type="text" />
+                                </FormGroup>
+                                <FormGroup className="contact__form">
+                                    <Input placeholder="Email" type="email" />
+                                </FormGroup>
+                                <FormGroup className="contact__form">
+                                    <textarea
+                                        rows="5"
+                                        placeholder="Message"
+                                        className="textarea form-control"
+                                    ></textarea>
+                                </FormGroup>
+
+                                <button className=" contact__btn" type="submit">
+                                    Gửi tin nhắn
+                                </button>
+                            </Form>
+                        </Col>
+
+                        <Col lg="5" md="5" className="description">
+                            <div className="contact__info">
+                                <h6 className="fw-bold">Thông tin liên hệ</h6>
+                                <p className="section__description mb-0">
+                                    Bird Farm Shop
+                                </p>
+                                <div className=" d-flex align-items-center gap-2">
+                                    <h6 className="fs-6 mb-0">Số điện thoại liên lạc:</h6>
+                                    <p className="section__description mb-0">0978286454</p>
+                                </div>
+
+                                <div className=" d-flex align-items-center gap-2">
+                                    <h6 className="mb-0 fs-6">Email:</h6>
+                                    <p className="section__description mb-0">birdfarmshop@gmail.com</p>
+                                </div>
+
+                                <h6 className="fw-bold mt-4">Theo dõi chúng tôi</h6>
+
+                                <div className=" d-flex align-items-center gap-4 mt-3">
+                                    {socialLinks.map((item, index) => (
+                                        <Link
+                                            to={item.url}
+                                            key={index}
+                                            className="social__link-icon"
+                                        >
+                                            <i class={item.icon}></i>
+                                        </Link>
+                                    ))}
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
         </div>
     );
-}
+};
 
 export default Contact;
