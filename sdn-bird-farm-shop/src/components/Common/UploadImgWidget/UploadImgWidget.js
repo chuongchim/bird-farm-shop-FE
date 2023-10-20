b9ddfc9c36b10624e21b7826e4c92c079baa3801
Thu Trang Le <lthutrang17@gmail.com>
2023-10-21 00:35:07 +0700

    remove unused code

--- sdn-bird-farm-shop/src/components/Common/UploadImgWidget/UploadImgWidget.js
+++ sdn-bird-farm-shop/src/components/Common/UploadImgWidget/UploadImgWidget.js
@@ -1,13 +1,3 @@
-// import React from "react";
-// // import { , useEffect, useRef } from 'react'
-// export default function UploadImgWidget() {
-//   const cloudinaryRef = useRef();
-//   useEffect(() => {
-//     cloudinaryRef.current = window.cloudinary;
-//   }, []);
-//   return <div>UploadImgWidget</div>;
-// }
-
 import React, { useEffect, useRef } from "react";
 import { Button } from "react-bootstrap";
 
