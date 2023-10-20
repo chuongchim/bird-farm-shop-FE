d90e6ba3199458943ad5fe85e333bd3bd29ae5e4
Thu Trang Le <lthutrang17@gmail.com>
2023-10-20 23:39:08 +0700

    Create upload image widget

--- sdn-bird-farm-shop/src/App.js
+++ sdn-bird-farm-shop/src/App.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Contact from './components/Pages/Contact/Contact';
-import About from './components/Pages/About/About';
-
-=======
 import React from 'react'; // Import React
 
 import './App.css';
@@ -15,29 +7,20 @@ import Register from './components/Authentication/Register/Register';
 import VerifyGmailForm from './components/Authentication/Verify/VerifyGmailForm';
 import HomeComponent from './components/Home/HomeComponent';
 import OrderPageComponent from './components/Order/OrderPage/OrderPageComponent';
->>>>>>> 3266307d091087d9015f2c89b5bf8f57bfe86a3a
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-<<<<<<< HEAD
-        <Routesz>
-          <Route path='/about' element={<About></About>}></Route>
-          <Route path='/contact' element={<Contact></Contact>}></Route>
-
-        </Routesz>
-=======
         <Routes>
           <Route path='/auth/login' element={<Login></Login>} />
           <Route path='/auth/register' element={<Register></Register>} />
           <Route path='/auth/verify' element={<VerifyGmailForm></VerifyGmailForm>}></Route>
           <Route path='/' element={<HomeComponent></HomeComponent>}></Route>
           <Route path='/order-page' element={<OrderPageComponent></OrderPageComponent>}></Route>
-          
-          
+
+
         </Routes>
->>>>>>> 3266307d091087d9015f2c89b5bf8f57bfe86a3a
       </BrowserRouter>
     </div>
   );
---
+++ sdn-bird-farm-shop/src/components/Common/UploadImgWidget/UploadImgWidget.js
@@ -0,0 +1,40 @@
+// import React from "react";
+// // import { , useEffect, useRef } from 'react'
+// export default function UploadImgWidget() {
+//   const cloudinaryRef = useRef();
+//   useEffect(() => {
+//     cloudinaryRef.current = window.cloudinary;
+//   }, []);
+//   return <div>UploadImgWidget</div>;
+// }
+
+import React, { useEffect, useRef } from "react";
+import { Button } from "react-bootstrap";
+
+function UploadImgWidget() {
+  const cloudinaryRef = useRef();
+  const widgetRef = useRef();
+  useEffect(() => {
+    cloudinaryRef.current = window.cloudinary;
+    widgetRef.current = cloudinaryRef.current.createUploadWidget({
+      cloudName: "diwpadoiz",
+      uploadPreset: "chuongchim",
+    }, (error, result) => {
+      if (!error && result && result.event === "success") {
+        console.log("Done! Here is the image info: ", result.info);
+      }
+    });
+  }, []);
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          widgetRef.current.open();
+        }}
+      >
+        Upload Image
+      </Button>
+    </div>
+  );
+}
+export default UploadImgWidget;
